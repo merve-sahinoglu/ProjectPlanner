95103fde17718d66f85473b0b077a58b83b62581
Merve Şahinoğlu <artemiswolfs@gmail.com>
2025-02-05 16:37:04 +0300

    yeni paket eklendi

---
+++ ui/src/hooks/useUserPreferenceStore.tsx
@@ -0,0 +1,32 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+import { i18n } from 'i18next';
+import Language from '../enum/language';
+
+export enum Theme {
+  Light = 'light',
+  Dark = 'dark',
+}
+
+interface UserPreferencesState {
+  // Language
+  language: Language;
+  changeLanguage: (language: Language, i18n: i18n) => Promise<void>;
+}
+
+const useUserPreferences = create(
+  persist<UserPreferencesState>(
+    (set, get) => ({
+      language: Language.Turkish,
+      changeLanguage: async (lang: Language, languageSwitcher: i18n) => {
+        set({ language: lang });
+        await languageSwitcher.changeLanguage(lang);
+      },
+    }),
+    {
+      name: 'user-preferences',
+    }
+  )
+);
+
+export default useUserPreferences;
