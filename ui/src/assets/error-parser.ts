dc9619f74e1d9d48c35cdfbf5943c9c492eaa4fc
Merve Şahinoğlu <artemiswolfs@gmail.com>
2025-02-05 15:28:56 +0300

    eksik dosyalar

---
+++ ui/src/assets/error-parser.ts
@@ -0,0 +1,8 @@
+const parseResponseErrors = (errors: { [key: string]: Array<string> }) => {
+  let validationError = Object.values(errors).toString();
+  validationError = validationError.replace(',', '\n');
+
+  return validationError;
+};
+
+export default parseResponseErrors;
